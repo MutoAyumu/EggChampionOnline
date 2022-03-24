e7f309fff8c091223f537c169ce992b63dc025c1
MutoAyumu <mutoayum0610@gmail.com>
2022-03-24 10:03:28 +0900

    [update]各レベルや購入状況を一つのクラスでまとめた

--- Assets/Scripts/Systems/LevelUpSystem.cs
+++ Assets/Scripts/Systems/LevelUpSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpSystem : MonoBehaviour
 {
@@ -10,10 +11,18 @@ public class LevelUpSystem : MonoBehaviour
     [SerializeField, Tooltip("Xが行　Yが列")] Vector2Int _matrix = default;
     int[,] _levelTable = default;
 
+    [SerializeField] int _eggNum = 0;
+    int _currentLevel;
+
+    [SerializeField] int _price = 200;
+    [SerializeField] Button _buyButton = default;
+    [SerializeField] GameObject _selectPanel = default;
+
+    [SerializeField] Canvas _canvas = default;
+
     [SerializeField] TMP_Text _levelUpMoneyText = default;
 
     [SerializeField] TMP_Text _levelText = default;
-    static int _currentLevel = 0;
 
     [SerializeField] TMP_Text _hpText = default;
     [SerializeField] TMP_Text _powerText = default;
@@ -47,26 +56,68 @@ public class LevelUpSystem : MonoBehaviour
     }
     private void Start()
     {
+        if (LevelUpManager.Buy[_eggNum])
+        {
+            _currentLevel = LevelUpManager.Levels[_eggNum];
+            _buyButton.gameObject.SetActive(false);
+            _selectPanel.SetActive(true);
+        }
+
         OnUiUpdate();
+    }
+    public void OnBuyEgg()
+    {
+        if(!LevelUpManager.Buy[_eggNum])
+        {
+            if(GameManager.Instance.Money >= _price)
+            {
+                GameManager.Instance.MoneyUpdate(_price);
+                _currentLevel = LevelUpManager.Instance.LevelUp(_eggNum);
+
+                LevelUpManager.Instance.BuyEgg(_eggNum);
+                _buyButton.gameObject.SetActive(false);
+                _selectPanel.SetActive(true);
+            }
+        }
     }
     public void OnLevelUp()
     {
         //読み込んだCSVの１列レベル行の値が今の所持金よりも多ければ
         if(GameManager.Instance.Money >= _levelTable[_currentLevel, 0] && _currentLevel != _matrix.x - 1)
         {
-            GameManager.Instance.Money -= _levelTable[_currentLevel, 0];
-            GameManager.Instance.MoneyUpdate();
-            _currentLevel++;
+            GameManager.Instance.MoneyUpdate(_levelTable[_currentLevel, 0]);
+
+            _currentLevel = LevelUpManager.Instance.LevelUp(_eggNum);
             OnUiUpdate();
         }
     }
     void OnUiUpdate()
     {
-        var level = _currentLevel + 1;
-        _levelText.text = level.ToString();
+        if (_levelTable[_currentLevel, 0] == 0)
+        {
+            _levelText.text = "Max";
+        }
+        else
+        {
+            var level = _currentLevel + 1;
+            _levelText.text = level.ToString();
+        }
+
         _levelUpMoneyText.text = _levelTable[_currentLevel, 0].ToString();
         _hpText.text = _levelTable[_currentLevel, 1].ToString();
         _powerText.text = _levelTable[_currentLevel, 2].ToString();
         _speedText.text = _levelTable[_currentLevel, 3].ToString();
+    }
+
+    public void OnCanvasActive()
+    {
+        if (_canvas.gameObject.activeSelf != true)
+        {
+            _canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            _canvas.gameObject.SetActive(false);
+        }
     }
 }
