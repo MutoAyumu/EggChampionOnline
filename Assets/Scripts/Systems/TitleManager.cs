3e2cceeb4ffd7e511338aa5152acae67d1cdd73c
MutoAyumu <mutoayum0610@gmail.com>
2022-03-31 04:34:58 +0900

    [add]NCMBを追加

--- Assets/Scripts/Systems/TitleManager.cs
+++ Assets/Scripts/Systems/TitleManager.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] TMP_InputField _nameField = default;
+    [SerializeField] TMP_InputField _passWordField = default;
+
+    bool isFirst;
+
 
 }
